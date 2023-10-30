0e6240225fc7da772cab7e3af264555775b9695c
Naym Hossen <naym@Nayms-MacBook-Pro.local>
2023-10-31 05:06:09 +0600

    done

--- Index.js
+++ Index.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const cookiParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-///////////// Midle Ware////////////
+///////////// Middle Ware////////////
 app.use(
   cors({
     origin: ["http://localhost:5173"],
-    credentials: true,
+    credential: true,
   })
 );
 app.use(express.json());
-app.use(cookiParser());
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_KEY}@firstpractice.poejscf.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,27 +27,26 @@ const client = new MongoClient(uri, {
 
 //////// ___Our Midle Ware___\\\\\\\\\
 
-const logger = async (req, res, next) => {
-  // console.log("Called", req.hostname, req.originalUrl);
-  next();
-};
+// const logger = async (req, res, next) => {
+//   // console.log("Called", req.hostname, req.originalUrl);
+//   next();
+// };
 
-const verifyToken = async (req, res, next) => {
-  const token = req.cookies?.token;
-  // console.log('my token', token);
-  if (!token) {
-    return res.status(401).send({ message: 'Not authorized' });
-  }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if(err){
-      return res.status(402).send({message:'unauthorized'});
-    }
-    // console.log('value the token', decoded);
-    req.user = decoded;
-    next();
-  })
-};
-
+// const verifyToken = async (req, res, next) => {
+//   const token = req.cookies?.token;
+//   // console.log('my token', token);
+//   if (!token) {
+//     return res.status(401).send({ message: "Not authorized" });
+//   }
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if (err) {
+//       return res.status(402).send({ message: "unauthorized" });
+//     }
+//     // console.log('value the token', decoded);
+//     req.user = decoded;
+//     next();
+//   });
+// };
 
 async function run() {
   try {
@@ -62,23 +59,25 @@ async function run() {
     // JWT auth Related Api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
+      console.log("user for token", user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
-      res
-        .cookie("token", token, {
-          httpOnly: true,
-          secure: false,
-        })
-        .send({ succss: true });
+      res.
+      cookie('Access Token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+      })
+      .send({success: true});
     });
 
     // Services Related API
-    app.get("/services", logger, async (req, res) => {
+    app.get("/services", async (req, res) => {
       const cursor = servicesCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.get("/services/:id", logger, verifyToken, async (req, res) => {
+    app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const options = {
@@ -90,11 +89,11 @@ async function run() {
 
     ///////////// Bookings//////////////
     ///////////// Bookings//////////////
-    app.get("/bookings", logger, verifyToken, async (req, res) => {
+    app.get("/bookings", async (req, res) => {
       // console.log('Main token', req.cookies.token);
       // console.log('from user valide token', req.user);
-      if(req.query.email !== req.query.email){
-        return res.status(401).send({message: 'forbidden access'})
+      if (req.query.email !== req.query.email) {
+        return res.status(401).send({ message: "forbidden access" });
       }
       let query = {};
       if (req.query?.email) {
@@ -104,7 +103,7 @@ async function run() {
       res.send(result);
     });
 
-    app.post("/bookings", verifyToken, async (req, res) => {
+    app.post("/bookings", async (req, res) => {
       const booking = req.body;
       const result = await bookingsCollection.insertOne(booking);
       res.send(result);
